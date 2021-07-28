76e28b41e1ef1f5457518cf6f128e3edbe5ebdf5
brunodassisf <brunodassis30@gmail.com>
2021-07-28 00:17:45 -0300

    Adjustment page modalacess

--- src/pages/ModalAcess/components/ModalAcess.styles.ts
+++ src/pages/ModalAcess/components/ModalAcess.styles.ts
@@ -1,71 +1,32 @@
-import styled from 'styled-components';
-import { colors } from '../../../theme/color';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: stretch;
-`;
-
-export const Wave = styled.div`
-    display: block;
-    position: absolute;
-    height: 65vh;
-    bottom: -1px;
-    background-image: url('data:image/svg+xml;utf8,<svg viewBox="0 0 1360 230" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(244, 241, 222, 1)" d="M 0 0 C 148.8 0 223.2 146 372 146 L 372 146 L 372 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 371 146 C 583.4 146 689.6 64 902 64 L 902 64 L 902 0 L 371 0 Z" stroke-width="0"></path> <path fill="rgba(47, 73, 94, 1)" d="M 901 64 C 1084.6 64 1176.4 163 1360 163 L 1360 163 L 1360 0 L 901 0 Z" stroke-width="0"></path></svg>');
-    background-size: cover;
-    background-repeat: no-repeat;
-    width: 100%;
-    z-index: 2;
-    background-color: #81b29a;
-`;
-
-export const Content = styled.div`
-    background-color: #ffffff;
-    border-radius: 10px;
-    z-index: 1;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    min-width: 600px;
-    position: absolute;
-    top: 25%;
-    border: 1px solid #ccc;
-    z-index: 3;
-    padding: 20px 0px;
-`;
-
-export const Divider = styled.div`
-    font-size: 0.6em;
-    color: ${colors.FONTS};
-    font-family: NunitoRegular;
-    font-weight: 600;
-    &:before {
-        content: '';
-        border-left: 1px solid ${colors.FONTS};
-        display: block;
-        height: 80px;
-        width: 1px;
-        margin: auto;
-        margin-bottom: 15px;
-    }
-
-    &:after {
-        content: '';
-        border-left: 1px solid ${colors.FONTS};
-        display: block;
-        height: 80px;
-        width: 1px;
-        margin: auto;
-        margin-top: 15px;
-    }
-`;
-
-export const Title = styled.h1`
-    color: ${colors.FONTS};
-    font-family: NunitoRegular;
-    font-weight: 700;
-    font-size: 1.2em;
-    position: absolute;
-    top: 16%;
-`;
+export const useStyles = makeStyles((theme) => ({
+  wave: {
+    display: 'block',
+    position: 'absolute',
+    bottom: '0px',
+    width: '100%',
+    height: '100vh',
+    backgroundImage: `url('data:image/svg+xml;utf8,<svg viewBox="0 0 1350 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(244, 241, 222, 1)" d="M 0 83 C 256 83 256 139 512 139 L 512 139 L 512 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 511 139 C 930.5 139 930.5 428 1350 428 L 1350 428 L 1350 0 L 511 0 Z" stroke-width="0"></path> </svg>')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: '#81b29a',
+    zIndex: -1,
+  },
+  gridContainer: {
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  appBar: {
+    marginBottom: theme.spacing(4),
+    borderTopLeftRadius: theme.spacing(3),
+    borderTopRightRadius: theme.spacing(3),
+    '& .MuiTab-wrapper': {
+      fontFamily: 'Nunito-800',
+    },
+  },
+  boxModal: {
+    borderRadius: theme.spacing(3),
+  },
+}));
--- src/pages/ModalAcess/components/ModalAcess.tsx
+++ src/pages/ModalAcess/components/ModalAcess.tsx
@@ -1,24 +1,103 @@
 import React from 'react';
-import { Container, Content, Divider, Title, Wave } from './ModalAcess.styles';
+
+import Grid from '@material-ui/core/Grid';
+
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '../../../components/Button';
+import Text from '../../../components/Text';
+
 import SignUp from '../../SignUp/components';
 import SignIn from '../../SignIn/components';
 
+import { useStyles } from './ModalAcess.styles';
+
+function TabPanel(props: {
+  [x: string]: any;
+  children: any;
+  value: any;
+  index: any;
+}) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && <>{children}</>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
 const ModalAcess = () => {
-    return (
-        <Container>
-            <Title>Junte, comece a fidelizar</Title>
-            <Content>
-                <div>
-                    <SignIn />
-                </div>
-                <Divider>ou</Divider>
-                <div>
-                    <SignUp />
-                </div>
-            </Content>
-            <Wave />
-        </Container>
-    );
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (
+    _event: any,
+    newValue: React.SetStateAction<number>,
+  ) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Grid container justifyContent="center" className={classes.gridContainer}>
+      <Grid item xs={11} md={5}>
+        <Text
+          shouldLoadColor="secondary"
+          weight="800"
+          align="center"
+          variant="h1"
+          mb={4}
+        >
+          Junte, comece a fidelizar
+        </Text>
+        <Box bgcolor="info.contrastText" pb={3} className={classes.boxModal}>
+          <Grid container justifyContent="space-around">
+            <AppBar
+              position="static"
+              color="default"
+              className={classes.appBar}
+            >
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                variant="fullWidth"
+                aria-label="full width tabs example"
+              >
+                <Tab label="Login" {...a11yProps(0)} />
+                <Tab label="Cadastre-se" {...a11yProps(1)} />
+              </Tabs>
+            </AppBar>
+
+            <Grid item xs={11} md={11}>
+              <TabPanel value={value} index={0}>
+                <SignIn />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                <SignUp />
+              </TabPanel>
+            </Grid>
+          </Grid>
+        </Box>
+      </Grid>
+      <Box className={classes.wave} />
+    </Grid>
+  );
 };
 
 export default ModalAcess;
