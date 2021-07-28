0a70a4b280899cbdc8da8f09c6f2021b88282222
brunodassisf <brunodassis30@gmail.com>
2021-07-28 00:17:34 -0300

    Adjustment page home

--- src/pages/Home/components/Home.styles.ts
+++ src/pages/Home/components/Home.styles.ts
@@ -1,19 +1,19 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-// import waveImg from '../../../assets/img/img01.svg';
-// import img01 from '../../assets/img/home-search.png';
-
 export const useStyle = makeStyles((theme) => ({
-    wave: {
-        display: 'block',
-        position: 'absolute',
-        bottom: '0px',
-        width: '100%',
-        height: '100vh',
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg viewBox="0 0 1350 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(244, 241, 222, 1)" d="M 0 83 C 256 83 256 139 512 139 L 512 139 L 512 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 511 139 C 930.5 139 930.5 428 1350 428 L 1350 428 L 1350 0 L 511 0 Z" stroke-width="0"></path> </svg>')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: '#81b29a',
-        zIndex: -1,
-    },
+  wave: {
+    display: 'block',
+    position: 'absolute',
+    bottom: '0px',
+    width: '100%',
+    height: '100vh',
+    backgroundImage: `url('data:image/svg+xml;utf8,<svg viewBox="0 0 1350 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(244, 241, 222, 1)" d="M 0 83 C 256 83 256 139 512 139 L 512 139 L 512 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 511 139 C 930.5 139 930.5 428 1350 428 L 1350 428 L 1350 0 L 511 0 Z" stroke-width="0"></path> </svg>')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: '#81b29a',
+    zIndex: -1,
+  },
+  gridContainer: {
+    height: '100vh',
+  },
 }));
--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -1,44 +1,67 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+
 import Button from '../../../components/Button';
 import Text from '../../../components/Text';
+
 import { useStyle } from './Home.styles';
+import ImgHome from '../../../assets/img/imgHome.svg';
 
 const Home: React.FC = () => {
-    const history = useHistory();
-    const classes = useStyle();
+  const history = useHistory();
+  const classes = useStyle();
 
-    const redirectAcess = () => {
-        history.push('/acesso');
-    };
+  const redirectAcess = () => {
+    history.push('/login');
+  };
 
-    return (
-        <Grid container alignItems="center" justifyContent="center">
-            <Grid item xs={12}>
-                <Text size={30} align="center">
-                    Comprando, você ja participa!
-                </Text>
-            </Grid>
-            <Grid item xs={12}>
-                <Text size={24} hasMarginBottom={4} weight="600" align="center">
-                    Ao comprar você já fideliza e participa!
-                </Text>
-            </Grid>
-            <Grid item xs={12}>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    weight="400"
-                    onClick={redirectAcess}
-                >
-                    Começar
-                </Button>
-            </Grid>
-            <Box className={classes.wave} />
-        </Grid>
-    );
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      className={classes.gridContainer}
+    >
+      <Grid item xs={12} md={5}>
+        <Text
+          shouldLoadColor="secondary"
+          weight="800"
+          variant="h1"
+          align="center"
+          mb={1}
+        >
+          Comprando, você ja participa!
+        </Text>
+
+        <Text
+          shouldLoadColor="secondary"
+          weight="600"
+          variant="h6"
+          align="center"
+          mb={3}
+        >
+          Ao comprar você já fideliza e participa!
+        </Text>
+        <Button
+          color="primary"
+          variant="contained"
+          align="center"
+          sizeText={20}
+          px={40}
+          onClick={redirectAcess}
+        >
+          Começar
+        </Button>
+      </Grid>
+      <Grid item xs={12} md={5}>
+        <img src={ImgHome} alt="" />
+      </Grid>
+      <Box className={classes.wave} />
+    </Grid>
+  );
 };
 
 export default Home;
