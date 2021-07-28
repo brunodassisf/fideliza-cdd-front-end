5476425770cb6587c1ecdcd6f4d7acdadf8a93d3
brunodassisf <brunodassis30@gmail.com>
2021-07-28 00:17:13 -0300

    Adjustment component dashboard

--- src/pages/Dashboard/components/Dashboard.styles.ts
+++ src/pages/Dashboard/components/Dashboard.styles.ts
@@ -1,59 +1,20 @@
-import styled from 'styled-components';
-import { IoCalendarOutline } from 'react-icons/io5';
-import { colors } from '../../../theme/color';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const Container = styled.div``;
-
-export const WaveMid = styled.div`
-    display: block;
-    position: absolute;
-    height: 50vh;
-    bottom: 50%;
-    background-image: url('data:image/svg+xml;utf8,<svg viewBox="0 0 1350 268" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(244, 241, 222, 1)" d="M 0 0 C 306 0 204 99 510 99 L 510 99 L 510 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 509 99 C 717.2 99 647.8 209 856 209 L 856 209 L 856 0 L 509 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 855 209 C 1035.6 209 975.4 248 1156 248 L 1156 248 L 1156 0 L 855 0 Z" stroke-width="0"></path><path fill="rgba(244, 241, 222, 1)" d="M 1155 248 C 1272 248 1233 177 1350 177 L 1350 177 L 1350 0 L 1155 0 Z" stroke-width="0"></path></svg>');
-    background-size: cover;
-    background-repeat: no-repeat;
-    width: 100%;
-    z-index: 2;
-    background-color: #81b29a;
-`;
-
-export const WaveRotate = styled.div`
-    display: block;
-    position: absolute;
-    height: 50vh;
-    bottom: 0%;
-    background-image: url('data:image/svg+xml;utf8,<svg viewBox="0 0 1350 268" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(244, 241, 222, 1)" d="M 0 0 C 306 0 204 99 510 99 L 510 99 L 510 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 509 99 C 717.2 99 647.8 209 856 209 L 856 209 L 856 0 L 509 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 855 209 C 1035.6 209 975.4 248 1156 248 L 1156 248 L 1156 0 L 855 0 Z" stroke-width="0"></path><path fill="rgba(244, 241, 222, 1)" d="M 1155 248 C 1272 248 1233 177 1350 177 L 1350 177 L 1350 0 L 1155 0 Z" stroke-width="0"></path></svg>');
-    background-size: cover;
-    background-repeat: no-repeat;
-    width: 100%;
-    z-index: 2;
-    transform: rotate(180deg);
-    background-color: #81b29a;
-`;
-
-export const Content = styled.div`
-    background-color: ${colors.WHITE};
-    height: 300px;
-    width: 300px;
-    max-width: 600px;
-    position: relative;
-    z-index: 3;
-    margin: auto;
-    border-radius: 20px;
-    border: 1px solid #ccc;
-`;
-
-export const Title = styled.h1`
-    color: ${colors.FONTS};
-    font-size: 1em;
-    font-family: NunitoRegular;
-    text-align: center;
-    margin-top: 22px;
-`;
-
-export const IconCalendar = styled(IoCalendarOutline)`
-    width: 100%;
-    margin-top: 5px;
-    color: ${colors.FONTS};
-    font-size: 1.4em;
-`;
+export const useStyles = makeStyles((theme) => ({
+  wave: {
+    display: 'block',
+    position: 'absolute',
+    bottom: '0px',
+    width: '100%',
+    height: '100vh',
+    backgroundImage: `url('data:image/svg+xml;utf8,<svg viewBox="0 0 1350 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(244, 241, 222, 1)" d="M 0 83 C 256 83 256 139 512 139 L 512 139 L 512 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(244, 241, 222, 1)" d="M 511 139 C 930.5 139 930.5 428 1350 428 L 1350 428 L 1350 0 L 511 0 Z" stroke-width="0"></path> </svg>')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: '#81b29a',
+    zIndex: -1,
+  },
+  boxUserInfos: {
+    width: 350,
+    margin: 'auto',
+  },
+}));
--- src/pages/Dashboard/components/Dashboard.tsx
+++ src/pages/Dashboard/components/Dashboard.tsx
@@ -1,26 +1,27 @@
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
 import React from 'react';
-import {
-    Container,
-    Content,
-    IconCalendar,
-    Title,
-    WaveMid,
-    WaveRotate,
-} from './Dashboard.styles';
+
 import Menu from '../../../components/Menu';
+import Text from '../../../components/Text';
+import { useStyles } from './Dashboard.styles';
 
 const Dashboard = () => {
-    return (
-        <Container>
-            <Menu />
-            <Content>
-                <Title>Calendário de fidelização</Title>
-                <IconCalendar />
-            </Content>
-            <WaveMid />
-            <WaveRotate />
-        </Container>
-    );
+  const classes = useStyles();
+  return (
+    <>
+      <div>
+        <Card className={classes.boxUserInfos}>
+          <Box bgcolor="info.contrastText">
+            <Text shouldLoadColor="secondary" weight="800" align="left">
+              Fulano
+            </Text>
+          </Box>
+        </Card>
+        <Box className={classes.wave} />
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
